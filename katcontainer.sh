4e08d7b4162f45dc949bbfb9a30ff7c8e681587e
Kat <10361870+katattakd@users.noreply.github.com>
2020-03-11 18:21:40 -0700

    Update katcontainer.sh

--- katcontainer.sh
+++ katcontainer.sh
@@ -20,13 +20,13 @@ export DEFAULT_MIRROR="http://dl-cdn.alpinelinux.org/alpine"
 export BOOTSTRAP_VERSION="v3.11"
 export BOOTSTRAP_VERSION_APK_TOOLS="2.10.4-r3"
 
-export DEFAULT_VERSION="v3.11"
+export DEFAULT_VERSION="latest-stable"
 ## All you need for a functional container. Package management is handled by the script, so having a package manager installed in the container isn't necessary.
-export DEFAULT_PACKAGES="busybox"
+export DEFAULT_PACKAGES="busybox dumb-init"
 ## The minimum set of packages you need to build stuff in a container. You'll probably want more than this.
-#export DEFAULT_PACKAGES="alpine-base alpine-sdk"
+#export DEFAULT_PACKAGES="alpine-base dumb-init alpine-sdk"
 ## The below list of packages is useful for developing containers or building stuff in them, but it's recommended that you use as few dependencies as possible for your finished container.
-#export DEFAULT_PACKAGES="alpine-base alpine-sdk coreutils bash byobu htop curl wget nano busybox-extras python perl autoconf cmake automake libtool"
+#export DEFAULT_PACKAGES="alpine-base dumb-init alpine-sdk coreutils bash byobu htop curl wget nano busybox-extras python perl autoconf cmake automake libtool"
 
 ## Default console width, works with basically all resoultions.
 export DEFAULT_CONSOLE_WIDTH="80"
@@ -352,7 +352,9 @@ generate_config () {
 				\"gid\": 0
 			},
 			\"args\": [
-				\"sh\", \"/init.sh\"
+				"/usr/bin/dumb-init",
+				"/bin/sh",
+				"/init.sh"
 			],
 			\"env\": [
 				\"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\",
